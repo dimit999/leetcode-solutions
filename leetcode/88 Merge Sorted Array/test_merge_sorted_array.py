9d37ea4c8102207a4287884e390f8cee053a7aba
dzmitryananyeu <DimITrY9D>
2025-05-01 23:51:32 +0400

    leetcode tasks 27 and 88

---
+++ leetcode/88 Merge Sorted Array/test_merge_sorted_array.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+class TestMergeSortedArray:
+
+    """
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+    
+    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+    
+     
+    
+    Example 1:
+    
+    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    Output: [1,2,2,3,5,6]
+    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+    """
+
+    data = [
+        ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]),
+        ([0], 0, [1], 1, [1]),
+        ([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3, [-1, 0, 0, 1, 2, 2, 3, 3, 3])
+    ]
+
+    @pytest.mark.parametrize("nums1, m, nums2, n, expected_list", data)
+    def test_merge_sorted_array(self, nums1, m, nums2, n, expected_list):
+        nums1[:] = sorted(nums1[:m] + nums2)
+        assert nums1[:] == expected_list, "Not correct expected result"
